039b93fc3ff6179984bbf547f4872ee30902f9de
roudai <me@roudai.net>
2023-04-16 23:36:47 +0900

    ランダムアイドル、グループツイート

--- src/group-tweet.ts
+++ src/group-tweet.ts
@@ -4,17 +4,21 @@ export class GroupTweet {
     constructor(private groupSheet: GoogleAppsScript.Spreadsheet.Sheet) {}
 
     randomGroupTweet() {
-        const line = Math.floor(
-            Math.random() * (this.groupSheet?.getLastRow() - 1) + 2
-        )
-        const schedule = dayjs.dayjs(getGroupSchedule(line))
-        const today = dayjs.dayjs()
-        if (schedule.isBefore(today) || schedule.isSame(today)) {
-            // 卒業・解散後の場合、再抽選
-            this.randomGroupTweet()
+        let tweetExecution = true
+        while (tweetExecution) {
+            const line = Math.floor(
+                Math.random() * (this.groupSheet?.getLastRow() - 1) + 2
+            )
+            const schedule = dayjs
+                .dayjs(getIdolSchedule(line))
+                .format('YYYY-MM-DD')
+            const today = dayjs.dayjs().format('YYYY-MM-DD')
+            if (schedule === '' || schedule > today) {
+                const group = getGroupName(line)
+                const twitter = 'https://twitter.com/' + getGroupTwitterID(line)
+                postTweet(`${group} ${twitter}`)
+                tweetExecution = false
+            }
         }
-        const group = getGroupName(line)
-        const twitter = 'https://twitter.com/' + getGroupTwitterID(line)
-        postTweet(`${group} ${twitter}`)
     }
 }
--- src/idol-tweet.ts
+++ src/idol-tweet.ts
@@ -4,18 +4,22 @@ export class IdolTweet {
     constructor(private idolSheet: GoogleAppsScript.Spreadsheet.Sheet) {}
 
     randomIdolTweet() {
-        const line = Math.floor(
-            Math.random() * (this.idolSheet?.getLastRow() - 1) + 2
-        )
-        const schedule = dayjs.dayjs(getIdolSchedule(line))
-        const today = dayjs.dayjs()
-        if (schedule.isBefore(today) || schedule.isSame(today)) {
-            // 卒業・解散後の場合、再抽選
-            this.randomIdolTweet()
+        let tweetExecution = true
+        while (tweetExecution) {
+            const line = Math.floor(
+                Math.random() * (this.idolSheet?.getLastRow() - 1) + 2
+            )
+            const schedule = dayjs
+                .dayjs(getIdolSchedule(line))
+                .format('YYYY-MM-DD')
+            const today = dayjs.dayjs().format('YYYY-MM-DD')
+            if (schedule === '' || schedule > today) {
+                const name = getIdolName(line)
+                const group = getIdolGroupName(line)
+                const twitter = 'https://twitter.com/' + getIdolTwitterID(line)
+                postTweet(`${name}（${group}）${twitter}`)
+                tweetExecution = false
+            }
         }
-        const name = getIdolName(line)
-        const group = getIdolGroupName(line)
-        const twitter = 'https://twitter.com/' + getIdolTwitterID(line)
-        postTweet(`${name}（${group}）${twitter}`)
     }
 }
